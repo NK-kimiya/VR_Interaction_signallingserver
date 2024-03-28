a70a58fa5d13df6ea4627fdd64fa7a786a9492c0
kimi <programassets6@gmail.com>
2024-03-28 15:33:48 +0900

    不要なコードの削除

--- chat_app/app.js
+++ chat_app/app.js
@@ -33,50 +33,7 @@ const io = new Server(http
       methods: ["GET", "POST"],
     },
   });
-
-const chat_socket = io.of('/chat');
-const video_socket = io.of('/video_socket');//追加
-const audio_chat_socket = io.of('/audio_chat_socket');
 const video_chat_socket = io.of('/video_chat_socket');
-const user_count = io.of('/user_count');
-
-chat_socket.on('connection', (socket) => {
-  socket.emit('allUsersInfo', Object.values(connectedUsers));
-  //ユーザー同士でデータの交換
-  socket.on('userInfo', (userInfo) => {
-    // ユーザー情報を保存
-    connectedUsers[socket.id] = userInfo;
-    // 新しいユーザー情報を全クライアントにブロードキャスト
-    chat_socket.emit('allUsersInfo', Object.values(connectedUsers));
-  });
-  socket.on('chatMessage', (message) => {
-    chat_socket.emit('chatMessage', message);
-  });
-  socket.on("join-room", (ROOM_ID, userId) => {
-    socket.join(ROOM_ID)
-    socket.to(ROOM_ID).emit("user-connected", userId)
-  });
-
-  socket.on('disconnect', () => {
-    socket.rooms.forEach((ROOM_ID) => {
-      socket.to(ROOM_ID).emit("user-disconnected", socket.id);
-    });
-    // connectedUsersからユーザーを削除し、更新情報をブロードキャスト
-    delete connectedUsers[socket.id];
-    chat_socket.emit('allUsersInfo', Object.values(connectedUsers));
-    chat_socket.emit('userDisconnected', socket.id); // イベント名を変更
-  });
-});
-
-audio_chat_socket.on("connection", socket => {
-  socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).emit("user-connected", userId);
-    socket.on("disconnect", () => {
-      socket.to(roomId).emit("user-disconnected", userId)
-    })
-  })
-})
 
 video_chat_socket.on("connection", socket => {
   console.log("受信完了");
@@ -100,32 +57,10 @@ video_chat_socket.on("connection", socket => {
       socket.to(toUserId).emit('ice-candidate', candidate);
     });
     socket.on("disconnect", () => {
-      print('切断されました')
       socket.to(roomId).emit("user-disconnected", userId)
     })
   })
 })
-
-video_socket.on("connection", socket => {
-  socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).emit("user-connected", userId);
-    socket.on("disconnect", () => {
-      socket.to(roomId).emit("user-disconnected", userId)
-    })
-  })
-})
-
-user_count.on('connection', (socket) => {
-  userCount++;
-  user_count.emit('userCountUpdate', userCount);
-
-  socket.on('disconnect', () => {
-    userCount--;
-    user_count.emit('userCountUpdate', userCount);
-  });
-});
-
 
 const port = process.env.PORT || 3000;
 http.listen(port || 3000, function () {
